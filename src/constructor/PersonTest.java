eb4b5db6d8b38089c5ba52d5ff6cc3610c14256e
goforit615 <bupark82@gsm.hs.kr>
2025-02-22 14:17:27 +0900

    test class

--- src/constructor/PersonTest.java
+++ src/constructor/PersonTest.java
@@ -2,6 +2,11 @@ package constructor;
 
 public class PersonTest {
     public static void main(String[] args) {
-        Person person = new Person();
+        Person personKim = new Person( );
+        personKim.name = "김구";
+        personKim.weight = 85.5F;
+        personKim.height = 180.0F;
+
+        Person personLee = new Person("이순신", 175, 175);
     }
 }
