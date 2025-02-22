32ad7a4fcceefef3c30f29b8f7466b703328b267
goforit615 <bupark82@gsm.hs.kr>
2025-02-22 10:02:14 +0900

    add StudentName function

--- src/classpart/StudentName.java
+++ src/classpart/StudentName.java
@@ -6,11 +6,18 @@ public class StudentName {
     int grade;
     String address;
 
-    public String getStudentName() {
+    public String getStudentName( ) {
         return studentName;
     }
 
-    public void setStudentName(String Name) {
-        studentName = Name;
+//    public void setStudentName(String Name) {
+//        studentName = Name;
+//    }
+    public static void main(String[ ] args) {
+        StudentName studentSon = new StudentName( );    // StudentName 클래스 생성
+        studentSon.studentName = "손범수";
+
+        System.out.println(studentSon.studentName);
+        System.out.println(studentSon.getStudentName( ));
     }
 }
