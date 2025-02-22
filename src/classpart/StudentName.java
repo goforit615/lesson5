8c2f71a9fcb43dc01fa4cdb9279a32e74e5cc452
goforit615 <bupark82@gsm.hs.kr>
2025-02-22 09:38:19 +0900

    StudentName

---
+++ src/classpart/StudentName.java
@@ -0,0 +1,12 @@
+package classpart;
+
+public class StudentName {
+    int studentID;
+    String studentName;
+    int grade;
+    String address;
+
+    public String getStudentName() {
+        return studentName;
+    }
+}
