25b4f3c733427cd9ec11d94694c83193fa34d0b1
goforit615 <bupark82@gsm.hs.kr>
2025-02-22 14:45:16 +0900

    access private variable

--- src/hiding/StudentTest.java
+++ src/hiding/StudentTest.java
@@ -3,7 +3,8 @@ package hiding;
 public class StudentTest {
     public static void main(String[] args) {
         Student studentLee = new Student();
-        studentLee.studentName = "이병헌";
+        //studentLee.studentName = "이병헌";
+        studentLee.setStudentName("이병헌");
 
         System.out.println(studentLee.getStudentName());
     }
